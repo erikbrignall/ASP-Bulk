f3ab2648c6ddd27298a48bf5e34973f9c29996e8
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-02-22 13:46:49 +0000

    Update streamlit_unitlist.py

--- streamlit_unitlist.py
+++ streamlit_unitlist.py
@@ -79,20 +79,21 @@ st.header('Modal Systems - Fetch Unit Ids')
     
 # SET PROPERTY TO FETCH UNIT IDS FOR AND WRITE TO DATAFRAME
 # Set ID for the property to find child units and names for
-parentName = st.text_input('Parent Name')
+#parentName = st.text_input('Parent Name')
 #roomTypeId = st.text_input('Parent ID')
 
 # Example dictionary for dropdown choice
-choice_dict = {"Calderon": "amzn1.alexa.unit.did.AEVDWFO5AUFK4VO7THLPMDTA332LQUIJUVFFD67Y54VQ6GCPXCP2MYIAOYUY5PZTHIQ4DLKHLU3ME5JJH3SYHJCJOMNYM5HMZPEUWQDJ", "Abascal": "amzn1.alexa.unit.did.AF2IN2KOHEXPX36FUNH5PJ36COOXFD5U2GNLSGK5NVAIOR3YQGSCSQPTY4G3UNX6Y5NKMJ3APX66YROFBD6ZPRSX5MV7YW7OP3BLFEZ2"}
+choice_dict = {
+    "Calderon": "amzn1.alexa.unit.did.AEVDWFO5AUFK4VO7THLPMDTA332LQUIJUVFFD67Y54VQ6GCPXCP2MYIAOYUY5PZTHIQ4DLKHLU3ME5JJH3SYHJCJOMNYM5HMZPEUWQDJ", 
+    "Abascal": "amzn1.alexa.unit.did.AF2IN2KOHEXPX36FUNH5PJ36COOXFD5U2GNLSGK5NVAIOR3YQGSCSQPTY4G3UNX6Y5NKMJ3APX66YROFBD6ZPRSX5MV7YW7OP3BLFEZ2"
+    }
 
 # Convert dictionary keys (or values) to a list for the dropdown
-options = list(choice_dict.keys())  # or list(choice_dict.values()) if you want the values instead
-
-# Taking a selection from the user with a dropdown
-selected_option = st.selectbox("Choose an option:", options)
+selected_key = st.selectbox("Select an option:", options=choice_dict.keys())
 
 # Set a variable based on the dropdown choice
-roomTypeId = choice_dict[selected_option]
+roomTypeId = choice_dict[selected_value]
+parentName = choice_dict[selected_key]
 
 
 if st.button('Submit'):
