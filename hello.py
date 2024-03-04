2c97b413804ae05de0a6275885f746f09c6add01
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-03-04 07:59:41 +0000

    Update hello.py
    add password verification to welcome page

--- hello.py
+++ hello.py
@@ -9,5 +9,40 @@ import hmac
 st.sidebar.success("Select from the options")
     
 st.header('Modal Systems - ASP Bulk Management Tools')
+
+############################
+# password module
+st.title('ASP Bulk Management tools')
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+############################
     
-st.write('Please select the action you would like to take. The possible functions are 1. List Campaigns, 2. Delete Specified Campaigns, 3. Get unit ID list by Hotel')
+st.write('Please select the action you would like to take on the left. The current functions are 1. Get unit IDs by hotel, 2. Get Campaign List, 3. Delete Campaigns')
