2ee81749c59df5d78aea785a9e5e9cdf03f17add
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-03-05 17:30:03 +0000

    Add files via upload

---
+++ pages/4_Create_Campaigns.py
@@ -0,0 +1,95 @@
+# imports
+import requests
+import pandas as pd
+import json
+import time
+import streamlit as st
+import hmac
+import io
+
+############################
+# password module
+st.title('ASP Bulk Management tools')
+
+#st.header('Enter password for a new token')
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+############################
+# FETCH UP TO DATE API TOKEN 
+url = "https://api.amazon.com/auth/o2/token"
+
+rtoken = st.secrets["rtoken"]
+client_id = st.secrets["client_id"]
+client_secret =  st.secrets["client_secret"]
+
+
+body = {
+        "refresh_token": f"{rtoken}",
+        "client_id":f"{client_id}",
+        "client_secret":f"{client_secret}",
+        "grant_type":"refresh_token"
+        }
+
+#st.write(body)
+
+try:
+    response = requests.post(url, json=body)
+
+    # Checking if the request was successful
+    if response.status_code == 200:
+        #st.write("Request successful!")
+        #print(response.text)
+        #st.write("token is:")
+        data = json.loads(response.text)
+        lwa_token = data['access_token']
+        #st.write(lwa_token)
+    else:
+        st.write("Request failed with status code:", response.status_code)
+        st.stop()
+except:
+    st.write("well that didn't work hmmm")
+    st.stop()
+
+####################################  
+st.header('Modal Systems - Bulk Add Campaigns - No Link')
+
+st.write("Please upload CSV file containing campaigns you wish to add")
+st.write("File must include headers: title, body, image, locale, start, end (e.g. es-ES")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    # To read csv file
+    stringio = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
+    df = pd.read_csv(stringio)
+    
+    st.dataframe(df, width=800)
+    
+    
+
