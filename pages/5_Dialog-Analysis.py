6f93afa0869944cc7ae0166abb8c89126fffd7cc
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-03-08 13:00:59 +0000

    Update 5_Dialog-Analysis.py

--- pages/5_Dialog-Analysis.py
+++ pages/5_Dialog-Analysis.py
@@ -76,78 +76,80 @@ end = st.date_input('End date')
 
 # Format the dates as strings in the specified format
 start = start_date.strftime('%Y-%m-%d')
+st.write(start)
 end = end_date.strftime('%Y-%m-%d')
+st.write(end)
 
-
-body = {"startTime": f"{start}", "endTime": f"{end}"}
-#initiate dataframe
-#columns=['userID','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
-columns=['userID','hotel','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
-
-dfFinal = pd.DataFrame(columns=columns)
-
-languages = ["en","es"]
-
-for lang in languages:
-
-    url = "https://api.modal.systems/analytics/channel/userinteraction/nhhotels-voice-" + lang
-
-    # Making a POST request with the cookie
-    response = requests.post(url, cookies=cookies, json=body)
-    parsed_json = json.loads(response.text)
-
-    rows = []
-
-    # Iterate through the JSON structure
-    for entry in parsed_json["data"]:
-        userId = entry['userId']
-        try:
-            hotel = entry['data']['propertyDetails']['property']['name']
-            #print(hotel)
-        except: 
-            hotel = "not shown"
-            #print(hotel)
-        for session_id, session in entry["sessions"].items():
-            for interaction in session["interactions"]:
-                # Extract necessary details
-                request_desc = interaction["request"]["description"]
-                request_intent = interaction["request"]["intent"]
-                request_slots = interaction["request"]["slots"]
-                request_locale = interaction["request"]["locale"]
-                request_timestamp = interaction["request"].get("timestamp", "")
-                response_text = interaction["response"]["outputSpeech"]
-                response_timestamp = interaction.get("timestamp", "")
-
-                # Append the details as a new row
-                rows.append([userId,hotel,session_id, request_desc, request_intent,request_timestamp, response_text, response_timestamp,request_slots,request_locale])
-
-    # Create a DataFrame
-    df = pd.DataFrame(rows, columns=["User ID","hotel","Session ID", "Request Description","Request Intent", "Request Timestamp", "Response Text", "Response Timestamp","Slots","Locale"])
-
-    #df = pd.DataFrame(rows, columns=["User ID","Session ID", "Request Description","Request Intent", "Request Timestamp", "Response Text", "Response Timestamp","Slots","Locale"])
-
-    df['Request Timestamp'] = pd.to_datetime(df['Request Timestamp'])
-
-    cols = ['userID','hotel','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
-
+if x == "1234:
+    body = {"startTime": f"{start}", "endTime": f"{end}"}
+    #initiate dataframe
+    #columns=['userID','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
+    columns=['userID','hotel','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
     
-    df.columns = cols
-    df = df.replace(r'\n|\r', '', regex=True)
-    dfFinal = dfFinal.append(df, ignore_index=True)
+    dfFinal = pd.DataFrame(columns=columns)
     
-    time.sleep(2)
-
-dfFinal['date'] = df['requestTimestamp'].dt.strftime('%d-%m-%Y')
-
-st.dataframe(dfRoom, width=800)
-
-
-def convert_df_to_csv(dfRoom):
-    return dfRoom.to_csv().encode('utf-8')
-
-st.download_button(
-    label="Download data as CSV",
-    data=convert_df_to_csv(dfA),
-    file_name='7-days-dialogs.csv',
-    mime='text/csv',
-    )
+    languages = ["en","es"]
+    
+    for lang in languages:
+    
+        url = "https://api.modal.systems/analytics/channel/userinteraction/nhhotels-voice-" + lang
+    
+        # Making a POST request with the cookie
+        response = requests.post(url, cookies=cookies, json=body)
+        parsed_json = json.loads(response.text)
+    
+        rows = []
+    
+        # Iterate through the JSON structure
+        for entry in parsed_json["data"]:
+            userId = entry['userId']
+            try:
+                hotel = entry['data']['propertyDetails']['property']['name']
+                #print(hotel)
+            except: 
+                hotel = "not shown"
+                #print(hotel)
+            for session_id, session in entry["sessions"].items():
+                for interaction in session["interactions"]:
+                    # Extract necessary details
+                    request_desc = interaction["request"]["description"]
+                    request_intent = interaction["request"]["intent"]
+                    request_slots = interaction["request"]["slots"]
+                    request_locale = interaction["request"]["locale"]
+                    request_timestamp = interaction["request"].get("timestamp", "")
+                    response_text = interaction["response"]["outputSpeech"]
+                    response_timestamp = interaction.get("timestamp", "")
+    
+                    # Append the details as a new row
+                    rows.append([userId,hotel,session_id, request_desc, request_intent,request_timestamp, response_text, response_timestamp,request_slots,request_locale])
+    
+        # Create a DataFrame
+        df = pd.DataFrame(rows, columns=["User ID","hotel","Session ID", "Request Description","Request Intent", "Request Timestamp", "Response Text", "Response Timestamp","Slots","Locale"])
+    
+        #df = pd.DataFrame(rows, columns=["User ID","Session ID", "Request Description","Request Intent", "Request Timestamp", "Response Text", "Response Timestamp","Slots","Locale"])
+    
+        df['Request Timestamp'] = pd.to_datetime(df['Request Timestamp'])
+    
+        cols = ['userID','hotel','sessionID','requestDescription','requestIntent','requestTimestamp','responseText','responseTimestamp','slots','locale']
+    
+        
+        df.columns = cols
+        df = df.replace(r'\n|\r', '', regex=True)
+        dfFinal = dfFinal.append(df, ignore_index=True)
+        
+        time.sleep(2)
+    
+    dfFinal['date'] = df['requestTimestamp'].dt.strftime('%d-%m-%Y')
+    
+    st.dataframe(dfRoom, width=800)
+    
+    
+    def convert_df_to_csv(dfRoom):
+        return dfRoom.to_csv().encode('utf-8')
+    
+    st.download_button(
+        label="Download data as CSV",
+        data=convert_df_to_csv(dfA),
+        file_name='7-days-dialogs.csv',
+        mime='text/csv',
+        )
