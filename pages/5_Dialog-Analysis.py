95c8abe88b4be40e0f0c32bf6ce7705ae049a6de
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-03-10 17:29:04 +0000

    Update 5_Dialog-Analysis.py

--- pages/5_Dialog-Analysis.py
+++ pages/5_Dialog-Analysis.py
@@ -170,7 +170,7 @@ if st.button('Click to fetch dialogs'):
         event_counts = summary_df['requestIntent'].value_counts()
         summary_df = pd.pivot_table(summary_df, values='requestTimestamp', index=['requestIntent'], aggfunc='count').rename(columns={'requestTimestamp': 'Count'})
         summary_df = summary_df.sort_values(by='Count', ascending=False)
-        minutes_dict = {'RoomServiceIntent': 10, ServiceRequestIntent': 10,'ServiceTimeRequestIntent': 10,'ConfirmOrderIntent': 10,'RequestAmenitiesIntent': 20, 'ConnectWifiIntent': 20, 'RoomDetailsIntent': 20, 'ReceptionIntent': 20, 'LaundryTypeServiceIntent': 20,'NewOrderIntent': 20,'ReceptionServiceTypeIntent': 20,'RequestAmenitiesCategoryIntent': 20}
+        minutes_dict = {'RoomServiceIntent': 10, 'ServiceRequestIntent': 10,'ServiceTimeRequestIntent': 10,'ConfirmOrderIntent': 10,'RequestAmenitiesIntent': 20, 'ConnectWifiIntent': 20, 'RoomDetailsIntent': 20, 'ReceptionIntent': 20, 'LaundryTypeServiceIntent': 20,'NewOrderIntent': 20,'ReceptionServiceTypeIntent': 20,'RequestAmenitiesCategoryIntent': 20}
 
         # Map prices to products using the dictionary
         summary_df['mins'] = summary_df['requestIntent'].map(minutes_dict).fillna(0)
